a676d2dbd056a65eb87291198f457328d633034f
yohann <yohann.tumpling@gmail.com>
2025-10-03 03:38:18 +0200

    feat(plugins): adds smart-splits plugin

---
+++ lua/plugins/smart-splits.nvim.lua
@@ -0,0 +1,14 @@
+return {
+	'mrjones2014/smart-splits.nvim',
+	-- config = function()
+	-- 	require("smpart-splits").setup({
+	-- 		-- extensions = {
+	-- 		-- 	smart_splits = {
+	-- 		-- 		mods = {
+	-- 		-- 			resize = '<M>'
+	-- 		-- 		}
+	-- 		-- 	}
+	-- 		-- }
+	-- 	})
+	-- end
+}
